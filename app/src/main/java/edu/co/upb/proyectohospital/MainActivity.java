55c5941cd0ea7f3d97ae436e09c5e6c4db60c271
Hlemusv <80725116+Hlemusv@users.noreply.github.com>
2021-11-30 16:29:43 -0500

    Creación del Archivo
    creación del archivo java para iniciar el performance para crear la aplicación y actualizar las versiones

---
+++ app/src/main/java/edu/co/upb/proyectohospital/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.co.upb.proyectohospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
