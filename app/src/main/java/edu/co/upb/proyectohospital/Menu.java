ce03f7ad9d574a170acb7b68faa890c3bbd81cb1
Hlemusv <80725116+Hlemusv@users.noreply.github.com>
2021-12-04 11:05:57 -0500

    Crear layout segun muestra en figma

---
+++ app/src/main/java/edu/co/upb/proyectohospital/Menu.java
@@ -0,0 +1,14 @@
+package edu.co.upb.proyectohospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
